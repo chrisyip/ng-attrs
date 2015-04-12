f2d4bdef2c2b662d64e2146017a063fa754b53e4
Chris Yip <i@chrisyip.im>
2015-04-13 04:26:52 +0800

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # ng-attrs
-Dynamic set or remove attributes in Angular
+
+Dynamic set or remove attributes in Angular, like `ngClass` for classes.
+
+# Install
+
+Bower:
+
+```
+bower install ng-attrs
+```
+
+Or manually download it.
+
+# Usage
+
+```html
+<!-- For unmin version -->
+<script src="bower_components/ng-attrs/ng-attrs.js"></script>
+<!-- For min version -->
+<script src="bower_components/ng-attrs/dist/ng-attrs.min.js"></script>
+<script>
+  angular.module('yourApp', ['ngAttrs'])
+</script>
+<body ng-app="yourApp">
+  <div ng-controller="yourCtrl">
+    <div ng-attrs="{ contenteditable: isEditable, 'custom-attr': isCustomAttr }"></div>
+  </div>
+</body>
+```
+
+# Todo
+
+- Support package managers
+- Source map
+- Tests
+
+# Author
+
+[Chris Yip](https://github.com/chrisyip)
---
+++ ng-attrs.js
@@ -0,0 +1,46 @@
+/**
+ * ng-attrs
+ * @license MIT
+ * @example
+ * <div ng-attrs="{ contenteditable: isEditable }"
+ * Use key-value pair, key will be attribute name, value will be the value.
+ * If value is a boolean value, then:
+ *   1. true - set attribute with '' value to element, this behavior is useful
+ *      for attribute that has no value, e.g. <div contenteditable></div>
+ *   2. false - remove attribute from element
+ * Otherwise, the value will be convert to a string
+ */
+
+~function (window, factory) {
+  'use strict'
+
+  if (!window.angular) {
+    return
+  }
+
+  // Initialize ngAttrs module
+  // @TODO support package manager, e.g. browserify, requirejs
+  factory(angular)
+}(window, function (angular) {
+  'use strict'
+
+  angular.module('ngAttrs', [])
+    .directive('ngAttrs', function () {
+      return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+          function ngAttrsWatchAction (value) {
+            angular.forEach(value, function (value, key) {
+              if (value === false) {
+                element.removeAttr(key)
+              } else {
+                element.attr(key, value === true ? '' : value)
+              }
+            })
+          }
+
+          scope.$watch(attrs.ngAttrs, ngAttrsWatchAction, true)
+        }
+      }
+    })
+});
